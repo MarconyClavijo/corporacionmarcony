ad4da16c97b8ee4df95d720b5bdbe624cb94ed7b
MarconyClavijo <marconyclavijo@hotmail.com>
2026-07-24 02:37:15 -0500

    Add service worker for file caching
    Implement service worker for caching files.

--- Billetera-Digital/Marcony-Clavijo/sw.js
+++ Billetera-Digital/Marcony-Clavijo/sw.js
@@ -1,1 +1,31 @@
+const CACHE_NAME = "billetera-marcony-v1";
 
+const ARCHIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./perfil.png",
+  "./Licencia.pdf",
+  "./TARJETA PROPIEDAD.pdf",
+  "./SOAT.pdf"
+];
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      return cache.addAll(ARCHIVOS);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
